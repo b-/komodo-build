9abd8b9bf85e7ad20ca8c9dd9d4fce8573fca11b
mbecker20 <becker.maxh@gmail.com>
2024-11-26 06:49:00 +0000

    Update upload-binaries.sh

--- upload-binaries.sh
+++ upload-binaries.sh
@@ -13,24 +13,24 @@ echo "Channel: ${CHANNEL}"
 echo "Tag: ${TAG}"
 echo "Version: ${KOMODO_VERSION}"
 
-# X86_64_IMAGE=git.komo.do/komodo/binaries:$TAG-x86_64
-# AARCH64_IMAGE=git.komo.do/komodo/binaries:$TAG-aarch64
+X86_64_IMAGE=git.komo.do/komodo/binaries:$TAG-x86_64
+AARCH64_IMAGE=git.komo.do/komodo/binaries:$TAG-aarch64
 
-# docker pull $X86_64_IMAGE
-# docker pull $AARCH64_IMAGE
+docker pull $X86_64_IMAGE
+docker pull $AARCH64_IMAGE
 
-# x86_64_id=$(docker create $X86_64_IMAGE sh)
-# aarch64_id=$(docker create $AARCH64_IMAGE sh)
+x86_64_id=$(docker create $X86_64_IMAGE sh)
+aarch64_id=$(docker create $AARCH64_IMAGE sh)
 
-# docker cp $x86_64_id:/periphery ./periphery-x86_64
-# docker cp $aarch64_id:/periphery ./periphery-aarch64
+docker cp $x86_64_id:/periphery ./periphery-x86_64
+docker cp $aarch64_id:/periphery ./periphery-aarch64
 
-# curl --user mbecker20:$KOMODO_GIT_TOKEN \
-#     --upload-file ./periphery-x86_64 \
-#     https://git.komo.do/api/packages/komodo/generic/periphery/$KOMODO_VERSION-$CHANNEL/periphery-x86_64
-# curl --user mbecker20:$KOMODO_GIT_TOKEN \
-#     --upload-file ./periphery-aarch64 \
-#     https://git.komo.do/api/packages/komodo/generic/periphery/$KOMODO_VERSION-$CHANNEL/periphery-aarch64
+curl --user mbecker20:$KOMODO_GIT_TOKEN \
+    --upload-file ./periphery-x86_64 \
+    https://git.komo.do/api/packages/komodo/generic/periphery/$KOMODO_VERSION-$CHANNEL/periphery-x86_64
+curl --user mbecker20:$KOMODO_GIT_TOKEN \
+    --upload-file ./periphery-aarch64 \
+    https://git.komo.do/api/packages/komodo/generic/periphery/$KOMODO_VERSION-$CHANNEL/periphery-aarch64
 
-# docker container rm $x86_64_id
-# docker container rm $aarch64_id
+docker container rm $x86_64_id
+docker container rm $aarch64_id
