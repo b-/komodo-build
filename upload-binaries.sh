598d9d938788192838a5450c12e3dd7766c6cf43
mbecker20 <becker.maxh@gmail.com>
2024-12-02 08:04:40 +0000

    Update upload-binaries.sh

--- upload-binaries.sh
+++ upload-binaries.sh
@@ -3,18 +3,18 @@
 # release or dev
 CHANNEL=$1
 # latest or latest-dev (or a specific version)
-TAG=$2
+BINARIES_IMAGE=$2
 # passed as komodo variable
 KOMODO_VERSION=$3
 # passed as komodo variable
 KOMODO_GIT_TOKEN=$4
 
 echo "Channel: ${CHANNEL}"
-echo "Tag: ${TAG}"
+echo "Image: ${BINARIES_IMAGE}"
 echo "Version: ${KOMODO_VERSION}"
 
-X86_64_IMAGE=git.komo.do/komodo/binaries:$TAG-x86_64
-AARCH64_IMAGE=git.komo.do/komodo/binaries:$TAG-aarch64
+X86_64_IMAGE=$BINARIES_IMAGE-x86_64
+AARCH64_IMAGE=$BINARIES_IMAGE-aarch64
 
 docker pull $X86_64_IMAGE
 docker pull $AARCH64_IMAGE
